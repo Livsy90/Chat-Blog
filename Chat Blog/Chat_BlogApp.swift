bc98ed04b963694a04f411160914a365f893d22c
Livsy90 <artmir@live.ru>
2022-11-29 18:45:02 +0400

    Initial Commit

---
+++ Chat Blog/Chat_BlogApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chat_BlogApp.swift
+//  Chat Blog
+//
+//  Created by Livsy on 29.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct Chat_BlogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
